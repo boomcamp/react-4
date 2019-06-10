b5fc0ae40c88888a5214fa9fac30992f9eb01e9e
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-06-10 15:12:03 -0600

    Add components

---
+++ src/components/ClassList/ClassList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class ClassList extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="box">
+        <h1 />
+        <h2>ClassList:</h2>
+      </div>
+    );
+  }
+}
