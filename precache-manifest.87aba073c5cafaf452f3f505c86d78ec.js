bb00d69047ce78e004487118f40020510b2c75a1
Aodhan Hayter <aodhan.hayter@gmail.com>
2019-06-11 13:10:30 -0600

    Updates

---
+++ precache-manifest.87aba073c5cafaf452f3f505c86d78ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38bd17e005b27266c83d8abf3d3cfb0",
+    "url": "/react-4/index.html"
+  },
+  {
+    "revision": "e4be42b939b3d300e001",
+    "url": "/react-4/static/css/main.56c51736.chunk.css"
+  },
+  {
+    "revision": "cd0dd5dc3012d3e7f4ed",
+    "url": "/react-4/static/js/2.0a05462a.chunk.js"
+  },
+  {
+    "revision": "e4be42b939b3d300e001",
+    "url": "/react-4/static/js/main.3a119512.chunk.js"
+  },
+  {
+    "revision": "266f3e29ff7c63d4a88d",
+    "url": "/react-4/static/js/runtime~main.4e9c8730.js"
+  }
+]);
