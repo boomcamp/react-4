01cf73ef17c212581af01afa4a012f780ce817d4
dianageromo <diana.geromo@boom.camp>
2019-06-21 17:36:51 +0800

    submission

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import back from './back.png';
 
 export default class Student extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      studentInfo: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:9090/students/${this.props.match.params.id}`)
+      .then(response => this.setState({ studentInfo: response.data }));
   }
 
   render() {
     return (
       <div className="box">
+        <Link to={`/classList/${this.state.studentInfo.class}`} >
+        <img src={back} alt = "back" width="50px" height="50px" /> 
+        </Link>
         <h1>Student</h1>
+        <h1>
+          {this.state.studentInfo.first_name} {this.state.studentInfo.last_name}
+        </h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
       </div>
     );
   }
