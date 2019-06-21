aad8aaf5f78adecd3a7eb4698d2a4a75fcd36b8b
Jan Patrick L. Bañares <janpatrick.banares@boom.camp>
2019-06-21 17:28:54 +0800

    Completed react-4 with Black Diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super();
+    this.state={
+      students: []
+    }
   }
 
+  componentDidMount(){
+    axios
+      .get(`http://localhost:9090/students?class=${this.props.match.params.class}`)
+      .then(res=>{
+        this.setState({
+          students: res.data,
+        });
+      })
+  }
   render() {
     return (
       <div className="box">
-        <h1 />
+        <Link to='/'>
+          <h3>Back</h3>
+        </Link>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        {this.state.students.map(stud=>(
+          <Link key = {stud.id} to ={`/student/${stud.id}`} class={this.props.match.params.class}>
+          <h3 >
+            {stud.first_name}, {stud.last_name}
+          </h3>
+          </Link>
+        ))}
       </div>
     );
   }
