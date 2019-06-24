a2c0e59bd0de112a97d3534b3169655a2b195169
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-06-24 18:00:46 +0800

    Finished but still working on the blak daymon.

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      students: [],
+    };
+
+  }
+
+  componentDidMount() {
+    axios
+    .get(`http://localhost:9090/students?class=${this.props.match.params.class}`)
+    .then(results => {
+      this.setState({
+        students: results.data,
+      });
+    });
   }
 
   render() {
     return (
       <div className="box">
-        <h1 />
+        <div>
+          <Link back={this.props.match.params.class} to="/">Home</Link>
+        </div>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        {this.state.students.map((student, i) => (
+          <Link key={student.id} to={`/student/${student.id}`}>
+            <h3 key={student.id}>
+              {student.first_name} {student.last_name}
+            </h3>
+          </Link>
+        ))}
       </div>
     );
   }
