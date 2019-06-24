e65d9e986e8bd5241874ee403203bf7485605c2a
Noel Layante <noel.layante@boom.camp>
2019-06-24 16:12:30 +0800

    Step 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Routes from './Routes';
 
@@ -11,8 +12,8 @@ export default class App extends Component {
         <nav className="nav">
           <div>WestSide University</div>
           <div className="link-wrap">
-            <div className="links">Home</div>
-            <div className="links">About</div>
+            <Link className="links" to="/">Home</Link>
+            <Link className="links" to="/about">About</Link>
           </div>
         </nav>
         <Routes />
