8b5d24d98458c54c0334f6e64a7c4d5b027d6ddc
marvin banton <marvin.banton@boom.camp>
2019-06-24 13:42:56 +0800

    Done Step 4

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact component={Home} path="/" />
       <Route component={About} path="/about" />
+      <Route component={ClassList} path="/classlist/:class" />
     </Switch>
   );
 }
