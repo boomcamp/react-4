f6c933cd54839ab2a9a61a7016f5a60a6b143808
noephilip-restum <noe.restum@boom.camp>
2019-11-14 17:44:17 +0800

    Initial Commit

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class ClassList extends Component {
   constructor() {
     super();
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:9090/students?class=${this.props.match.params.class}`
+      )
+      .then(response => {
+        this.setState({ students: response.data });
+      });
   }
 
   render() {
     return (
       <div className="box">
-        <h1 />
+        <button onClick={() => window.history.back()}>Back</button>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        {this.state.students.map(student => (
+          <Link key={student.id} to={`/student/${student.id}`}>
+            <h3>
+              {student.first_name} {student.last_name}
+            </h3>
+          </Link>
+        ))}
       </div>
     );
   }
